ad29f2e37b809c4ce5528375e0aef25d08488b37
v0idpwn <v0idpwn@gmail.com>
2019-11-19 23:50:11 -0200

    add Dialyxir and Excoveralls dependencies

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule FinancialSystem.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -21,8 +23,8 @@ defmodule FinancialSystem.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:dialyxir, "~> 0.5", only: [:dev]},
+      {:excoveralls, "~> 0.10", only: [:test]}
     ]
   end
 end
